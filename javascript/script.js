0edeaea56adf16913867cc4e293bc24cd311192c
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2021-12-23 08:35:06 +0700

    progress bar sudah selesai dan tambah section career

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+// buat icon menu bar jika di pencet akan berubah
+
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
 
@@ -6,10 +8,13 @@ menu.onclick = () => {
     header.classList.toggle('active');
 }
 
+// membuat navbar ilang jika sudah di pencet
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
+
+// buat efek tema malam
 
 let themeToggler = document.querySelector('#theme-toggler');
 
@@ -21,3 +26,24 @@ themeToggler.onclick = () => {
         document.body.classList.remove('active');
     }
 }
+
+// membuat scroll spy
+
+// let section = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll = () => {
+//     section.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height){
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector("header nav a[href*=' + id + ']").classList.add('active');
+//             });
+//         };
+//     });
+// };
